d0e4414310e94a4494f275244dc9cdf6bff7beb7
student <student@FCA608-10>
2022-05-20 14:24:45 +0900

    プロジェクト ファイルを追加します。

---
+++ move_window.cpp
@@ -0,0 +1,19 @@
+﻿#include <windows.h>
+#include <stdlib.h>
+
+BOOL CALLBACK WndEnumProc(HWND hWnd, LPARAM)
+{
+    char szClassName[256];
+    ::GetClassNameA(hWnd, szClassName, _countof(szClassName));
+    for (int i=0; ::lstrcmpiA(szClassName, "Notepad") == 0;i++) {
+            MoveWindow(hWnd, i, i, i , i , TRUE);
+            Sleep(16);
+    }
+    return TRUE;
+}
+
+int main()
+{
+    ::EnumWindows(WndEnumProc, 0);
+    return 0;
+}
