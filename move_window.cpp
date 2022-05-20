053188d67eb83327831e489b1f52d8805d1bb15e
ゲーム共有 ＦＣＡ <fca-game@edu.fca.ac.jp>
2022-05-20 14:33:21 +0900

    kannsei

--- move_window.cpp
+++ move_window.cpp
@@ -1,12 +1,17 @@
 ﻿#include <windows.h>
 #include <stdlib.h>
-
+#include <random>
+std::random_device rnd;
+std::mt19937 mt(rnd());     //  メルセンヌ・ツイスタの32ビット版、引数は初期シード値
+std::uniform_int_distribution<> rand1080(0, 1080);        // [0, 99] 範囲の一様乱数
+std::uniform_int_distribution<> rand720(0, 720);        // [0, 99] 範囲の一様乱数
 BOOL CALLBACK WndEnumProc(HWND hWnd, LPARAM)
 {
     char szClassName[256];
     ::GetClassNameA(hWnd, szClassName, _countof(szClassName));
-    for (int i=0; ::lstrcmpiA(szClassName, "Notepad") == 0;i++) {
-            MoveWindow(hWnd, i, i, i , i , TRUE);
+    for (; ::lstrcmpiA(szClassName, "Notepad") == 0;) {
+        
+            MoveWindow(hWnd, rand1080(mt), rand720(mt), 500,  500, TRUE);
             Sleep(16);
     }
     return TRUE;
@@ -14,6 +19,7 @@ BOOL CALLBACK WndEnumProc(HWND hWnd, LPARAM)
 
 int main()
 {
+    WinExec("notepad", SW_SHOW);
     ::EnumWindows(WndEnumProc, 0);
     return 0;
 }
